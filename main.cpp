3a3cf9050c237e3e4c1bef7c28dad787968de2ec
calyptr4 <alessandrabaires@gmail.com>
2026-03-27 19:01:23 -0700

    a54

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //	Lab 5-11 : Problem Definition
-//	Make a program that find the prime numbers in the given range.
-//		example) find the prime numbers in the range [1..100]
+//	Make a program that find the prime nbers in the given range.
+//		example) find the prime nbers in the range [1..100]
 //		2, 3, 5, 7, ... , 97
 
 #include <iostream>
@@ -8,19 +8,22 @@ using namespace std;
 
 int main()
 {
-	int begin, end;
+	int begin, end, n, i;
 
 	do
 	{
 		cout << "Enter your range [from, to] \n";
 		cin >> begin >> end;
 	} while (begin >= end || begin < 2 || end < 0);
-
-	// TODO: iterate through each number in the range [begin..end]
-	// TODO: check if n is prime by testing divisibility from 2 up to n-1
-	// TODO: if n is divisible by i, it is not prime
-	// TODO: if n is prime, print it
-
+	
+	for(n = begin; n <= end; n++){
+		for (i=2; i<n; i++){
+		if (n%i == 0)
+			break;
+		}
+	if (i == n)
+	cout << n << " ";
+  }
 	cout << endl;
 
 	return 0;
